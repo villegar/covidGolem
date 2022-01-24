c4b9c3b92c6813d1253fb3ad0830a29c92cdbc03
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-01-24 23:26:47 +0000

    Add deployment scripts

---
+++ R/deploy.R
@@ -0,0 +1,26 @@
+# library(rsconnect)
+# a function to stop the script when one of the variables cannot be found. and to strip quotation marks from the secrets when you supplied them. (maybe it is just easier to never use them)
+error_on_missing_name <- function(name) {
+  var <- Sys.getenv(name, unset = NA)
+  if (is.na(var)) {
+    stop(paste0("cannot find ", name, " !"), call. = FALSE)
+  }
+  gsub("\"", '', var)
+}
+
+# Authenticate
+rsconnect::setAccountInfo(
+  name   = error_on_missing_name("SHINY_ACC_NAME"),
+  token  = error_on_missing_name("SHINY_TOKEN"),
+  secret = error_on_missing_name("SHINY_SECRET")
+)
+
+# Deploy the application.
+rsconnect::deployApp(
+  # appDir = "R",
+  appFiles = "app.R",
+  # appFiles = c("R/app_ui.R"), #, you can specify which files to deploy,
+               #or keep this NULL to deploy everything),
+  appName = error_on_missing_name("SHINY_MAINNAME"),
+  appTitle = "shinyapplication"
+)
